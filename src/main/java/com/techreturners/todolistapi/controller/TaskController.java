bad61314e3dc2df7bbe327e17ed81c66485f4df6
Amjad <amjadshakir@gmail.com>
2023-10-22 17:09:52 +0100

    feat: exception handling for create task

--- src/main/java/com/techreturners/todolistapi/controller/TaskController.java
+++ src/main/java/com/techreturners/todolistapi/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.techreturners.todolistapi.controller;
 
 import com.techreturners.todolistapi.model.Task;
 import com.techreturners.todolistapi.service.TaskService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,8 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TaskController {
     @Autowired
     TaskService taskService;
+
     @PostMapping
-    public ResponseEntity<Task> addTask(@RequestBody Task task){
+    public ResponseEntity<Task> addTask(@RequestBody @Valid Task task){
         Task newTask = taskService.insertTask(task);
         return new ResponseEntity<>(newTask, HttpStatus.CREATED);
     }
---
+++ src/main/java/com/techreturners/todolistapi/model/ValidationError.java
@@ -0,0 +1,13 @@
+package com.techreturners.todolistapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ValidationError {
+    private String field;
+    private String errorMessage;
+}
